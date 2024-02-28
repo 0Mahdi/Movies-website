83be3d720667dab56b12e8579e3abb371f712783
Mahdi Hossen <114096228+0Mahdi@users.noreply.github.com>
2024-02-28 10:10:40 +0600

    completed

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'swiper/css'
+import './App.css';
+import Banner from './pages/Banner';
+import Header from './pages/Header';
+import Main from './pages/Main';
+import Footer from './pages/Footer';
+import BackToTopBtn from './components/BackToTopBtn';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [scroll, setScroll] =useState(0);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () =>{
+      setScroll(window.scrollY);
+    });
+    return () => {
+      window.removeEventListener('scroll', () => {
+        setScroll(window.screenY);
+      });
+    };
+  }, [scroll]);
+  return (
+    <>
+      <Header scroll={scroll} />
+      <Banner />
+      <Main />
+      <Footer/>
+      <BackToTopBtn scroll={scroll}/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Header.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './header.css';
+import NavListItem from '../components/NavListItem';
+import navListData from '../data/navListData';
+import Search from '../components/Search';
+import Button from '../components/Button';
+
+function Header({scroll}) {
+  const [navList, setNavList] = useState(navListData);
+
+  const handleNavOnClick = id => {
+    const newNavList = navList.map(nav => {
+      nav.active = false;
+      if (nav._id) nav.active = true;
+      return  nav;
+    })
+  }
+  return (
+    <header className={`${scroll>100 ? 'scrolled' : undefined}`}>
+      <a href='/' className='logo'>Moviez</a>
+      <ul className='nav'>
+      {navList.map(nav => (
+        <NavListItem key={nav._id} nav={nav} navOnClick={handleNavOnClick} />
+      ))}
+    </ul>
+    <Search />
+    <Button 
+      icon={<ion-icon name="log-in-outline"></ion-icon>}
+      name="Sign In"
+     />
+    </header>
+  );
+}
+
+export default Header;
+
